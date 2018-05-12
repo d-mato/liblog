568bbdd5161b0494a449001e1acee972a5dfad68
Daiki Matoba <telnetstat@gmail.com>
2018-05-12 11:37:08 +0900

    Implement search form

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -3,6 +3,17 @@ class LoansController < ApplicationController
 
   def index
     @loans = current_user.loans.eager_load(:library).order(ended_at: :desc)
+
+    @q = params[:q]
+    if @q.present?
+      pattern = "%#{@q}%"
+      condition = [
+        Loan.arel_table[:book_title].matches(pattern),
+        Loan.arel_table[:author].matches(pattern),
+        BookReview.arel_table[:comment].matches(pattern)
+      ].inject { |memo, c| memo.or(c) }
+      @loans = @loans.includes(:book_review).where(condition)
+    end
   end
 
   def show
