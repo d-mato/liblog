856dd5e19bb8492c4d2f2d5abbb99a8814869156
Daiki Matoba <telnetstat@gmail.com>
2018-05-12 14:07:50 +0900

    Reset puma conf

--- app/controllers/admin/crawler_workers_controller.rb
+++ app/controllers/admin/crawler_workers_controller.rb
@@ -2,7 +2,7 @@ class Admin::CrawlerWorkersController < ApplicationController
   protect_from_forgery except: :create
 
   def create
-    LibraryUser.pluck(:id).each { |id| CrawlerJob.perform_later(id) }
+    LibraryUser.pluck(:id).each { |id| CrawlerJob.perform_now(id) }
     head :ok
   end
 end
