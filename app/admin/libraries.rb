a56de31d22c7e6e59ba42a03ca637062c031fbd0
Daiki Matoba <telnetstat@gmail.com>
2018-05-13 11:19:52 +0900

    Add admin crud

---
+++ app/admin/libraries.rb
@@ -0,0 +1,3 @@
+ActiveAdmin.register Library do
+  permit_params :name, :crawler
+end
--- app/admin/loans.rb
+++ app/admin/loans.rb
@@ -1,3 +1,24 @@
 ActiveAdmin.register Loan do
   permit_params :started_at, :ended_at, :place_name, :book_title, :author, :isbn, :last_fetched_at, :returned
+
+  controller do
+    def scoped_collection
+      Loan.includes(:user, :library)
+    end
+  end
+
+  index do
+    id_column
+    column(:user) { |loan| link_to loan.user.email, [:admin, loan.user] }
+    columns :book_title
+    column :started_at
+    column :ended_at
+    column(:library) { |loan| link_to loan.library.name, [:admin, loan.library] }
+    column :place_name
+    column :author
+    column :isbn
+    column :last_fetched_at
+    column :returned
+    actions
+  end
 end
