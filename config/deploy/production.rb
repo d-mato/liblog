cdb95a7b4abf0a45ff622552a82a71bc5b334755
Daiki Matoba <telnetstat@gmail.com>
2018-08-01 08:19:38 +0900

    Update deploy destination

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,13 @@
-server '153.122.58.221', roles: %w(app db web)
+server 'ec2', roles: %w(app db web)
 
 set :rbenv_ruby, '2.5.0'
 set :puma_threads, [0, 1]
+
+task :prepare_bundle_config do
+  on roles(:app) do
+    execute :bundle, :config, 'build.pg', '--with-pg-config=/usr/pgsql-10/bin/pg_config'
+  end
+end
+before 'bundler:install', 'prepare_bundle_config'
 
 after 'deploy:publishing', 'ridgepole:apply'
