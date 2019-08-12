f435266365a447624e2a57deb78bc7ca04adf725
Daiki Matoba <telnetstat@gmail.com>
2019-08-13 01:05:41 +0900

    Fix test for faker

--- spec/factories/loans.rb
+++ spec/factories/loans.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :loan do
     user
     library
-    started_at { Faker::Date.between(14.days.ago, Date.today) }
-    ended_at { Faker::Date.between(Date.tomorrow, 14.days.since) }
+    started_at { Faker::Date.between(from: 14.days.ago, to: Date.today) }
+    ended_at { Faker::Date.between(from: Date.tomorrow, to: 14.days.since) }
     last_fetched_at { Date.today }
     book_title { Faker::Book.title }
   end
