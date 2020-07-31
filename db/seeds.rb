2f1a86fd9ed74ba4d9e6b614bb450b6f990cc00b
Daiki Matoba <telnetstat@gmail.com>
2020-08-01 01:08:07 +0900

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,4 +15,7 @@ ActiveRecord::Base.transaction do
 
   library = Library.create!(name: '江戸川区立図書館	', crawler: 'Crawler::EdogawaCrawler')
   user.library_users.create!(library: library, sign_in_id: '9005499104', password: 'lib8848')
+
+  library = Library.create!(name: '品川区立図書館	', crawler: 'Crawler::ShinagawaCrawler')
+  user.library_users.create!(library: library, sign_in_id: '16086884', password: 'book8848')
 end
