10e15a626a95578388efc4ef7f5190ee983e9251
Daiki Matoba <telnetstat@gmail.com>
2021-03-09 20:51:09 +0900

    Create ExtensionsController

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -1,6 +1,6 @@
 class LoansController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_loan, only: %i[show extend_loan set_returned]
+  before_action :set_loan, only: %i[show set_returned]
 
   def index
     @loans = current_user.loans.includes(:library, :book_review).order(ended_at: :desc)
@@ -19,22 +19,6 @@ class LoansController < ApplicationController
   def show; end
 
   def calendar; end
-
-  # 延長
-  def extend_loan
-    library_user = current_user.library_users.find_by!(library: @loan.library)
-    crawler = @loan.library.crawler.constantize.new(id: library_user.sign_in_id, password: library_user.password)
-
-    crawler.login
-
-    begin
-      crawler.extend_loan(@loan.book_title)
-      # CrawlerJob.perform_later(library_user.id)
-      redirect_to @loan, notice: '延長しました！返却期限は後ほど更新されます'
-    rescue Crawler::CannotExtendError
-      redirect_to @loan, alert: '延長処理に失敗しました。お手数ですが図書館Webサイトにログインして確認してください。'
-    end
-  end
 
   # 返却済みにする
   def set_returned
