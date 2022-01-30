ce31b77217a79bbff4efea7067370ae4703672fb
Daiki Matoba <telnetstat@gmail.com>
2022-01-30 14:33:35 +0900

    Update styles for bootstrap5

--- app/javascript/components/loans.vue
+++ app/javascript/components/loans.vue
@@ -2,12 +2,12 @@
   <div class="list-group py-4">
     <router-link :to="`/loans/${loan.id}`" v-for="loan in loans" class="list-group-item text-decoration-none" :style="{ background: loan.returned ? '' : 'aliceblue' }">
       <div class="d-flex align-items-center">
-        <i class="far fa-comment-alt mx-1" v-if="loan.book_review" data-toggle="tooltip" title="レビューあり"></i>
-        <div class="font-weight-bold">{{loan.book_title}}</div>
-        <div class="text-right small">
-          <div class="badge badge-secondary mx-1" v-if="loan.returned">返却済</div>
-          <div class="badge badge-info mx-1" v-if="!loan.returned">{{loan.ended_at}}</div>
-          <div class="d-inline ml-1 text-muted">{{loan.library.name}}</div>
+        <i class="far fa-comment-alt mx-1" v-if="loan.book_review" data-bs-toggle="tooltip" title="レビューあり"></i>
+        <div class="fw-bold">{{ loan.book_title }}</div>
+        <div class="text-end small">
+          <div class="badge bg-secondary mx-1" v-if="loan.returned">返却済</div>
+          <div class="badge bg-info mx-1" v-if="!loan.returned">{{ loan.ended_at }}</div>
+          <div class="d-inline ms-1 text-muted">{{ loan.library.name }}</div>
         </div>
       </div>
     </router-link>
@@ -15,16 +15,18 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        loans: []
-      }
-    },
-    mounted() {
-      fetch('/loans.json').then(res => res.json()).then(data => {
+export default {
+  data() {
+    return {
+      loans: []
+    }
+  },
+  mounted() {
+    fetch('/loans.json')
+      .then(res => res.json())
+      .then(data => {
         this.loans = data
       })
-    }
   }
+}
 </script>
--- app/javascript/libs/bootstrap.js
+++ app/javascript/libs/bootstrap.js
@@ -1,6 +1,1 @@
-import jQuery from 'jquery'
 import 'bootstrap'
-
-jQuery(function () {
-  jQuery('[data-toggle="tooltip"]').tooltip()
-})
